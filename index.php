da76035af662e39fb985d49200e0e9b09135d5a7
unknown <puigmarti99@gmail.com>
2018-11-28 17:41:36 +0100

    Added header and footer

--- index.php
+++ index.php
@@ -10,14 +10,67 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
-      
+ 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-  <nav class="navbar navbar-dark bg-dark">
-  <!-- Navbar content -->
-</nav>
+  
+
+  <!-- Barra de navegació -->
+  <nav class="navbar  navbar-expand-lg navbar-dark bg-dark">
+  <!-- botó expansió navbar versió móbil -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+  <!--Div del navbar -->
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+   <!--Link i nom de la pàgina -->
+    <a class="navbar-brand" href="#">TrapAdivisor</a>
+   <!--Llista amb elements del navbar -->
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+   <!--Primer element de la llista -->
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+   <!--Segon element de la llista -->
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+   <!--Tercer element de la llista -->   
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    
+    <!-- Formulari del quadre de cerca 
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+        -->
+  </div>
+</nav>  
+<!-- Titol -->
+    <div class="text-center bg-faded">
+    <h1> TrapAdvisor </h1>
+    <h2 class="text-muted"> The best page to find places you SHOULDN'T go!! </h2>
+    </div>
+    <hr class="bg-dark col-xs-12">
+
+
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <p> Esto lo he robado que flipas, pero diremos que lo ha hecho Marti Puig</p>
+    <a href="https://github.com/Puig99"> Mi github </a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</body>
 </html>
