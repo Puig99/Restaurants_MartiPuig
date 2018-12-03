5ba565e69e4766e89b28ba25384200ebb259b9c9
unknown <puigmarti99@gmail.com>
2018-12-03 17:25:43 +0100

    array content shown in screen

--- index.php
+++ index.php
@@ -10,6 +10,91 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
+  <!-- PHP -->
+  <!-- ARRAYS -->
+<?php
+  $restaurants=array(
+                      array(
+                      "name"=>"Milo cocina mestiza",
+                      "locality"=>"Palma de Mallorca",
+                      "street"=>"Calle Caro nº30",
+                      "postal"=>"07013",
+                      "phone"=>"8710040413",
+                      "image"=>"Foto",
+                      "commtitle"=>"El peor restaurante de la HISTORIA",
+                      "comment"=>"Tras muchos años de probar restaurantes, buenos y malos, pensaba que lo peor ya lo habría visto, que nada me podía sorprender, pero me equivocaba, este es sin duda el PEOR RESTAURANTE EN EL QUE HE ESTADO EN TODA MI VIDA"
+                        ),
+                      array(
+                      "name"=>"La nueva Burguesa",
+                      "locality"=>"Palma de Mallorca",
+                      "street"=>"Calle Sant Magi nº76",
+                      "postal"=>"07013",
+                      "phone"=>"871509530",
+                      "image"=>"foto",
+                      "commtitle"=>"Es un ascooo",
+                      "comment"=>"Estaba comiendo de lo mas rico. Hasta que me hechi una pisca de picante y ne sale una mosca dentro del aceite que ascoooooo encontrarne con ese bichoooo" 
+                        ),
+                      array(
+                        "name"=>"Norice Palma",
+                        "locality"=>"Palma de Mallorca",
+                        "street"=>"Plaça de Sant Antoni nº17",
+                        "postal"=>"07002",
+                        "phone"=>"871115308",
+                        "image"=>"foto",
+                        "commtitle"=>"La paella más mala y cara que me he comido jamás",
+                        "comment"=>"Es una vergüenza que aún queden restaurantes que timan a sus clientes con platos como paellas hechas con producto congelado e insulso hervido en agua disgustada dando como resultado un paella sin sabor e indecentemente cara. Vaya jeta. Pésimo y nada recomendable." 
+                         ),
+                      array(
+                        "name"=>"Daikiya",
+                        "locality"=>"Palma de Mallorca",
+                        "street"=>"Carrer de Jaume Balmes nº63",
+                        "postal"=>"07004",
+                        "phone"=>"871708557",
+                        "image"=>"foto",
+                        "commtitle"=>"El peor restaurante de Mallorca",
+                        "comment"=>"Creo que este restaurante sólo lo define esa palabra 'pésimo'  fui a una comunión, pedí solomillo y hubiera preferido comerme la suela de mi zapato, los camareros borrachos una vergüenza. Si odias a alguien recomiendale este restaurante" 
+                      ),
+                      array(
+                        "name"=>"Mas39",
+                        "locality"=>"Palma de Mallorca",
+                        "street"=>"Calle de La Fabrica nº13",
+                        "postal"=>"07013",
+                        "phone"=>"871044623",
+                        "image"=>"foto",
+                        "commtitle"=>"Decepcionante , tardaré en volver",
+                        "comment"=>"He sido cliente regular de la barra de pinchos, nada que objetar sobre la comida . 
+                        Pero tengo que decir que el último día tuve muy mala suerte. Ninguneo y falta de profesionalidad por parte del personal como pocas veces y en pocos sitios he vivido , ya no hablemos de amabilidad ni de empatía que brillaron por su ausencia.
+                        Una pena." 
+                      ));
+  function getRestaurants(){
+    global $restaurants;
+    foreach($restaurants as $key =>$value){
+      ?>
+       <!-- Card on es mostra la informació dels arrays -->
+       <div class="col-md-7">
+        <a href="#">
+          <img class="img-fluid rounded mb-3 mb-md-0" src="<?= $value["imatge"] ?>/700x300" alt="imatge restaurant">
+        </a>
+      </div>
+      <div class="col-md-5">
+      <ul>
+        <!-- Nom restaurant -->
+        <li>  <h3><?= $value["name"] ?></h3>  </li>
+        <!-- Localitat -->
+        <li>  <h5><?= $value["locality"] ?> </h5> </li>
+        <li>  <h5> <?= $value["street"] ?></h5> </li>
+        <li>  <h5> <?= $value["postal"] ?> </h5> </li>
+        <li>  <h5> <?= $value["phone"] ?> </h5> </li>
+        <li>  <h5> <?= $value["commtitle"] ?> </h5>  </li>
+        <li>  <p> <?= $value["comment"] ?> </p>  </li>
+        </ul>
+      </div>
+      <br>
+      <br>
+    <?php
+    }
+  }
+                      ?>
  
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -59,71 +144,11 @@
     </div>
     <hr class="bg-dark">
 
-<!-- ARRAYS -->
-<?php
-$restaurants=array(
-                  array(
-                        "name"=>"Milo cocina mestiza",
-                        "locality"=>"Palma de Mallorca",
-                        "street"=>"Calle Caro nº30",
-                        "postal"=>"07013",
-                        "phone"=>"8710040413",
-                        "image"=>"Foto"
-                      ),
-                  array(
-                        "namw"=>"La nueva Burguesa",
-                        "locality"=>"Palma de Mallorca",
-                        "street"=>"Calle Sant Magi nº76",
-                        "postal"=>"07013",
-                        "phone"=>"871509530",
-                        "image"=>"foto"
-                      ),
-                  array(
-                        "name"=>"Norice Palma",
-                        "locality"=>"Palma de Mallorca",
-                        "street"=>"Plaça de Sant Antoni nº17",
-                        "postal"=>"07002",
-                        "phone"=>"871115308",
-                        "image"=>"foto"
-                      ),
-                  array(
-                        "name"=>"Daikiya",
-                        "locality"=>"Palma de Mallorca",
-                        "street"=>"Carrer de Jaume Balmes nº63",
-                        "postal"=>"07004",
-                        "phone"=>"871708557",
-                        "image"=>"foto"
-                      ),
-                  array(
-                    "name"=>"Mas39",
-                    "locality"=>"Palma de Mallorca",
-                    "street"=>"Calle de La Fabrica nº13",
-                    "postal"=>"07013",
-                    "phone"=>"871044623",
-                    "image"=>"foto"
-                  ));
-                  ?>
 <div class="container">
 <div class="row">
-        <div class="col-md-7">
-          <a href="#">
-            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="imatge restaurant">
-          </a>
-        </div>
-        <div class="col-md-5">
-        <ul>
-          <!-- Nom restaurant -->
-          <li>  <h3>Nom</h3>  </li>
-          <!-- Localitat -->
-          <li>  <h5>Localitat</h5> </li>
-          <li>  <h5> descripcio </h5> </li>
-          <li>  <h5> Direcció </h5> </li>
-          <li>  <h5> Codi postal </h5> </li>
-          <li>  <h5> Telefon </h5>  </li>
-          </ul>
-        </div>
-      </div>
-    </div> 
+<?php  getRestaurants(); ?>
+</div>
+</div>
 <!-- Footer -->
 <footer class="page-footer font-small blue">
 
